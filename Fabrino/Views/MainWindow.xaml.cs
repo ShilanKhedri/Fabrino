887f8153a8dfe834f4da1648be97444ebd4d84c6
Shilan <shilan.khedri8@gmail.com>
2025-04-06 20:24:51 +0330

    implementing SignUp UI

---
+++ Fabrino/Views/MainWindow.xaml.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using Microsoft.Data.SqlClient;
+using System.Windows;
+using Fabrino.Controllers;
+using Fabrino.Models;
+using System.Windows.Input;
+using Fabrino.Views;
+
+namespace Fabrino
+{
+    public partial class MainWindow : Window
+    {
+        // رشته اتصال به دیتابیس 
+        private AuthController authController = new AuthController();
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void GoToRegister_Click(object sender, MouseButtonEventArgs e)
+        {
+            SignUpWindow registerWindow = new SignUpWindow();
+            registerWindow.Show();
+            this.Close(); // یا Hide اگه بخوای فرم لاگین باز بمونه
+        }
+
+        private void RemoveText(object sender, RoutedEventArgs e)
+        {
+            var tb = sender as System.Windows.Controls.TextBox;
+            if (tb != null && tb.Text == "نام کاربری")
+            {
+                tb.Text = "";
+                tb.Foreground = System.Windows.Media.Brushes.Black;
+            }
+        }
+
+        private void AddText(object sender, RoutedEventArgs e)
+        {
+            var tb = sender as System.Windows.Controls.TextBox;
+            if (tb != null && string.IsNullOrWhiteSpace(tb.Text))
+            {
+                tb.Text = "نام کاربری";
+                tb.Foreground = System.Windows.Media.Brushes.Gray;
+            }
+        }
+
+        private void RemovePassword(object sender, RoutedEventArgs e)
+        {
+            var pb = sender as System.Windows.Controls.PasswordBox;
+            if (pb != null && pb.Password == "رمز عبور")
+            {
+                pb.Clear();
+                pb.Foreground = System.Windows.Media.Brushes.Black;
+            }
+        }
+
+        private void AddPassword(object sender, RoutedEventArgs e)
+        {
+            var pb = sender as System.Windows.Controls.PasswordBox;
+            if (pb != null && string.IsNullOrWhiteSpace(pb.Password))
+            {
+                pb.Password = "رمز عبور";
+                pb.Foreground = System.Windows.Media.Brushes.Gray;
+            }
+        }
+
+
+        private void LoginButton_Click(object sender, RoutedEventArgs e)
+        {
+            var user = new UserModel
+            {
+                Username = UsernameTextBox.Text.Trim(),
+                PasswordHash = PasswordTextBox.Password.Trim()
+            };
+
+            if (string.IsNullOrEmpty(user.Username) || string.IsNullOrEmpty(user.PasswordHash))
+            {
+                MessageBox.Show("لطفاً نام کاربری و رمز عبور را وارد کنید.", "خطا", MessageBoxButton.OK, MessageBoxImage.Warning);
+                return;
+            }
+
+            if (authController.IsValidUser(user))
+            {
+                MessageBox.Show("ورود موفق!", "موفقیت", MessageBoxButton.OK, MessageBoxImage.Information);
+            }
+            else
+            {
+                MessageBox.Show("نام کاربری یا رمز عبور اشتباه است!", "خطا", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
+        }
+    }
+
+}
+
