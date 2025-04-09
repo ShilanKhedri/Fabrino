ab7084ca7f8c15563b20637e826cb8f864c6b91a
Shilan <shilan.khedri8@gmail.com>
2025-04-09 19:21:17 +0330

    imlementing forgotpassword

---
+++ Fabrino/Views/ForgotPasswordStep1.xaml.cs
@@ -0,0 +1,53 @@
+﻿using Fabrino.Controllers;
+using Fabrino.Views;
+using System.Windows;
+
+namespace Fabrino.Views
+{
+    public partial class ForgotPasswordStep1 : Window
+    {
+        public ForgotPasswordStep1()
+        {
+            InitializeComponent();
+        }
+
+        
+        private void RemoveText(object sender, RoutedEventArgs e)
+        {
+            var tb = sender as System.Windows.Controls.TextBox;
+            if (tb != null && tb.Text == "نام کاربری")
+            {
+                tb.Text = "";
+                tb.Foreground = System.Windows.Media.Brushes.Black;
+            }
+        }
+
+        private void AddText(object sender, RoutedEventArgs e)
+        {
+            var tb = sender as System.Windows.Controls.TextBox;
+            if (tb != null && string.IsNullOrWhiteSpace(tb.Text))
+            {
+                tb.Text = "نام کاربری";
+                tb.Foreground = System.Windows.Media.Brushes.Gray;
+            }
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            string username = UsernameTextBox.Text;
+            var controller = new ForgotPasswordController();
+            string question = controller.GetSecurityQuestion(username);
+
+            if (question != null)
+            {
+                ForgotPasswordStep2 step2 = new ForgotPasswordStep2(username, question);
+                step2.Show();
+                this.Close();
+            }
+            else
+            {
+                MessageBox.Show("کاربری با این نام یافت نشد.");
+            }
+        }
+    }
+}
---
+++ Fabrino/Views/ForgotPasswordStep2.xaml.cs
@@ -0,0 +1,59 @@
+﻿using System.Windows;
+using Microsoft.Data.SqlClient;
+using System.Configuration;
+using Fabrino.Helpers;
+using Fabrino.Controllers;
+
+namespace Fabrino.Views
+{
+    public partial class ForgotPasswordStep2 : Window
+    {
+        string username;
+        public ForgotPasswordStep2(string user, string question)
+        {
+            InitializeComponent();
+            username = user;
+            AnswerTextBox.Text = question;
+            
+            
+        }
+
+
+        private void RemoveText(object sender, RoutedEventArgs e)
+        {
+            var tb = sender as System.Windows.Controls.TextBox;
+            if (tb != null && tb.Text == "پاسخ")
+            {
+                tb.Text = "";
+                tb.Foreground = System.Windows.Media.Brushes.Black;
+            }
+        }
+
+        private void AddText(object sender, RoutedEventArgs e)
+        {
+            var tb = sender as System.Windows.Controls.TextBox;
+            if (tb != null && string.IsNullOrWhiteSpace(tb.Text))
+            {
+                tb.Text = "پاسخ";
+                tb.Foreground = System.Windows.Media.Brushes.Gray;
+            }
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            string answer = QuestionTextBox.Text;
+            var controller = new ForgotPasswordController();
+
+            if (controller.ValidateSecurityAnswer(username, answer))
+            {
+                ForgotPasswordStep3 step3 = new ForgotPasswordStep3(username);
+                step3.Show();
+                this.Close();
+            }
+            else
+            {
+                MessageBox.Show("پاسخ نادرست است.");
+            }
+        }
+    }
+}
